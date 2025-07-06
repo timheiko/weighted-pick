dca70d63e2fa8ddadfb0bd6afe78d3b296cdda6c
Tim Heiko <175552092+timheiko@users.noreply.github.com>
2025-07-06 16:23:12 +0200

    Make WeightedPick class fields private
    Made `WeightedPick` class fields private
    
    Test plan:
    ```shell
    % npm run test && npm run lint
    ...
     ✓ __tests__/WeightedPick.test.js (5 tests) 4ms
       ✓ WeightedPick > 2 values, 50%/50%, 1K times 2ms
       ✓ WeightedPick > 2 values, 33%/66%, 1K times 0ms
       ✓ WeightedPick > 5 values, 20%/20%/20%/20%/20%, 1K times 0ms
       ✓ WeightedPick > 5 values, 10%/10%/30%/25%/25%, 1K times 0ms
       ✓ WeightedPick > prod values, 1K times 0ms
    
     Test Files  1 passed (1)
          Tests  5 passed (5)
    ...
    ```

--- index.js
+++ index.js
@@ -8,37 +8,39 @@ class WeightedValue {
 }
 
 class WeightedPick {
+    #weights;
+    #ranges;
 
     constructor(weights) {
         // TODO: Check for empty input data.
-        this.weights = weights;
+        this.#weights = weights;
 
         // [[start1, end1), [start2, end2), ...]
         const totalWeight = weights.reduce((acc, w) => acc + w.weight, 0);
 
-        this.ranges = [];
+        this.#ranges = [];
         let runningSum = 0;
         for (const {weight} of weights) {
             runningSum += weight;
-            this.ranges.push(runningSum / totalWeight);
+            this.#ranges.push(runningSum / totalWeight);
         }
     }
 
     pickValue() {
         const rnd = Math.random();
         let lo = 0;
-        let hi = this.ranges.length - 1;
+        let hi = this.#ranges.length - 1;
 
         // Binary Search
         while (lo <= hi) {
             const mid = Math.floor((hi - lo) / 2) + lo;
-            if (rnd < this.ranges[mid]) {
+            if (rnd < this.#ranges[mid]) {
                 hi = mid - 1;
             } else {
                 lo = mid + 1;
             }
         }
-        return this.weights[lo].value;
+        return this.#weights[lo].value;
     }
 
     pickValues(n) {
